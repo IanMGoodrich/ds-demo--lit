3f9e50c06a20a6820e915d735a093176e5ce0389
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2025-05-19 12:47:58 -0700

    feat(social-share): Update to match var changes.

--- src/components/social-share/img-social-share-styles.ts
+++ src/components/social-share/img-social-share-styles.ts
@@ -21,9 +21,8 @@ export const socialShareStyles = css`
   --border-radius: 8px;
   --border-width: 2px;
   --border-color: var(--cl-border-floral);
-  --action-color-primary: var(--action-color-primary-floral);
-  --action-color-secondary: var(--action-color-secondary-floral);
-  --background-color: var(--cl-background-primary-floral);
+  --action-color-primary: var(--cl-bold-tertiary-floral);
+  --background-color: var(--cl-background-primary-floral);    
 }
 
 :host([theme='sprawl']) {
@@ -31,8 +30,7 @@ export const socialShareStyles = css`
   --border-width: 1px;
   --border-radius: 0px;
   --border-color: var(--cl-border-sprawl);
-  --action-color-primary: var(--action-color-secondary-sprawl);
-  --action-color-secondary: var(--action-color-primary-sprawl);
+  --action-color-primary: var(--cl-bold-primary-sprawl);
   --background-color: var(--cl-background-primary-sprawl);
 }
 
@@ -41,8 +39,7 @@ export const socialShareStyles = css`
   --border-radius: 4px;
   --border-width: 2px;
   --border-color: var(--cl-border-default);
-  --action-color-primary: var(--action-color-primary-default);
-  --action-color-secondary: var(--action-color-secondary-default);
+  --action-color-primary: var(--cl-bold-tertiary-default);
   --background-color: var(--cl-background-primary-default);
 }
 
@@ -55,7 +52,7 @@ export const socialShareStyles = css`
 
   &:focus {
     --svg-color: var(--action-color-primary);
-    outline: solid 2px var(--action-color-secondary);
+    outline: solid 2px var(--action-color-primary);
     outline-offset: 2px;
   }
 }
